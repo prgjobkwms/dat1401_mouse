770ccdcb7c0e1a98f75c46c2e0d57f65f59c2d27
kuwabara <kw7101410@gmail.com>
2014-06-20 15:35:48 +0900

    timeとラベルの反射と停止

--- mouse/Form1.Designer.cs
+++ mouse/Form1.Designer.cs
@@ -32,12 +32,16 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // timer1
             // 
             this.timer1.Enabled = true;
-            this.timer1.Interval = 64;
+            this.timer1.Interval = 32;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // label1
@@ -58,11 +62,55 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "･ﾟ･(ﾉД＼lll)･ﾟ･";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "配列練習";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "for文練習";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 9);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(691, 9);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(3, 0, 30, 0);
+            this.label3.Size = new System.Drawing.Size(68, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(632, 438);
+            this.ClientSize = new System.Drawing.Size(762, 491);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -77,6 +125,10 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- mouse/Form1.cs
+++ mouse/Form1.cs
@@ -11,53 +11,138 @@ namespace mouse
 {
     public partial class Form1 : Form
     {
-        //ラベルの移動変数
-        int iVelX = rand.Next(-10,11);
-        int iVelY = rand.Next(-10,11);
+        int chrnum;
+        int time;
+        
+        //定数宣言
+        const int TEKI_MAX = 10;
+        const int SPEED_MAX = 11;
+        const int SPEED_MIN = -10;
+        //const int chrnum = TEKI_MAX;
+             
+        //ラベルの表示個数と移動変数
+        Label[] chrs = new Label[TEKI_MAX];
+        int[] iVX = new int[TEKI_MAX];
+        int[] iVY = new int[TEKI_MAX];
+
         //rand変数
         private static Random rand = new Random();
+        //ラベルの移動変数
+        int iVelX = rand.Next(SPEED_MIN,SPEED_MAX);
+        int iVelY = rand.Next(SPEED_MIN,SPEED_MAX);
 
         public Form1()
         {
+            chrnum = TEKI_MAX;
+            time = 0;
+
             InitializeComponent();
+            /*コンストラクタ
+             * Form1クラスが生成される時に実行する特別な変数
+             */
+            //ラベルの生成
+            for (int i = 0; i < 10; i++)
+            {
+                //ラベルのステータス
+                chrs[i] = new Label();
+                chrs[i].AutoSize = true;//ミソ
+                chrs[i].Text = "|電柱|◞౪◟◞≼●≽◟)";
+                chrs[i].Left = rand.Next(ClientSize.Width);
+                chrs[i].Top = rand.Next(ClientSize.Height);
+                chrs[i].Font = new Font("メイリオ", 8);
+                chrs[i].ForeColor = Color.FromArgb(150, 80, 00);
+                Controls.Add(chrs[i]);//フォームに追加
+                //移動変数
+                iVX[i] = rand.Next(SPEED_MIN, SPEED_MAX);
+                iVY[i] = rand.Next(SPEED_MIN, SPEED_MAX);
+            }
+
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            //time+キャラを全部消したら止める
+            if (chrnum > 0)
+            {
+                time++;
+                label3.Text = "Time:" + time;
+            }
             //-------------------マウス追従--------------------------
-            //2次元クラスPoint型（かた）の変数（へんすう）cposを宣言（せんげん）
+            //2次元クラスPoint型の変数cposを宣言
             Point cpos;
 
-            //cposにマウスのフォーム座標（ざひょう）を取り出す（とりだす）
+            //cposにマウスのフォーム座標を取り出す
             cpos = this.PointToClient(MousePosition);
 
-            //フォームにマウス座標（ざひょう）を表示（ひょうじ）
+            //フォームにマウス座標を表示
             Text = "" + cpos.X + "," + cpos.Y;
 
-            //マウス座標（ざひょう）にラベルをくっつける。(マウスの座標（ざひょう）をlabelに代入（だいにゅう）)
+            //マウス座標にラベルをくっつける。(マウスの座標をlabelに代入）
             label1.Left = cpos.X;
             label1.Top = cpos.Y;
 
             //------------------ラベル2の移動-------------------------
-            //ラベル2の移動（いどう）
+            //ラベル2の移動値
             int vx = iVelX;
             int vy = iVelY;
-            // ラベルの座標（ざひょう）に加算（かさん）
-            label2.Left += vx;
-            label2.Top += vy;
+            // 移動値をラベルの座標に加算
+            label2.Left += iVelX;
+            label2.Top += iVelY;
 
             if ((label2.Left < 0) || (label2.Left + label2.Width > ClientSize.Width))
             {
-                //左右反転（さゆうはんてん）
+                //左右反転
                 label2.Left -= vx;
                 iVelX = -vx;
             }
-            else
             if ((label2.Top < 0) || (label2.Top + label2.Height > ClientSize.Height))
             {
-                //上下反転（じょうげはんてん）
+                //上下反転
                 label2.Top -= vy;
                 iVelY = -vy;
+            }
+            //------------------for制作のラベルの移動-------------------------
+            
+            //ラベルの移動
+            for (int i = 0; i < TEKI_MAX; i++)
+            {
+                //キャラが有効か
+                if (chrs[i].Visible == false) 
+                {
+                    continue;
+                }
+
+                chrs[i].Left += iVX[i];
+                chrs[i].Top += iVY[i];
+
+                //ラベルの移動
+                if ((chrs[i].Left < 0) || (chrs[i].Left + chrs[i].Width > ClientSize.Width))
+                {
+                    //左右反転
+                    chrs[i].Left -= iVX[i];
+                    iVX[i] = -iVX[i];
+                }
+                
+                if ((chrs[i].Top < 0) || (chrs[i].Top + chrs[i].Height > ClientSize.Height))
+                {
+                    //上下反転）
+                    chrs[i].Top -= iVY[i];
+                    iVY[i] = -iVY[i];
+                }
+                //
+                if ((cpos.X >= chrs[i].Left)
+                    && (cpos.X < chrs[i].Left + chrs[i].Width)
+                    && (cpos.Y >= chrs[i].Top)
+                    && (cpos.Y < chrs[i].Top + chrs[i].Height))
+                {
+                    chrs[i].Visible = false;  //消える
+                    //クリア表示
+                    chrnum--;
+                    if (chrnum == 0) 
+                    {
+                        MessageBox.Show("クリア");
+                    }
+                }
             }
 
             //-------------------ラベル1とラベル2の衝突判定------------------------
@@ -76,12 +161,56 @@ namespace mouse
             }
 
         }
+        
 
+        //------------------------ボタン練習（game関係なし）-------------------------------------
+        //配列練習ボタン
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //int型の配列変数を３つ定義
+            int[] iar = new int [4];
+            //[]の中に添え字を入れることで別の場所にアクセスできる
+            iar[0] = 0;
+            iar[1] = 1;
+            iar[2] = 2;
+            MessageBox.Show(iar[0].ToString());
+            MessageBox.Show(iar[1].ToString());
+            MessageBox.Show(iar[2].ToString());
+            //添え字には変数も使うことができる
+            int i = 0;
+            MessageBox.Show(iar[i].ToString());
+        }
 
-        //テキストbox2のプログラム変更なしでよい
-        private void textBox2_TextChanged(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            for (i = 0; i < 10; i++)
+            {
+                if (i < 3)
+                {
+                    continue;
+                }
+                else
+                if (i >= 6)
+                {
+                    break;
+                }
+                MessageBox.Show(i.ToString());
+            }
+            MessageBox.Show("i =" + i);
+        }
+
+        private void label3_Click(object sender, EventArgs e)
         {
 
         }
+
+        //ラベル２再起動ボタン
+       private void button3_Click(object sender, EventArgs e)
+        {
+             iVelX = rand.Next(SPEED_MIN, SPEED_MAX);
+             iVelY = rand.Next(SPEED_MIN, SPEED_MAX);
+        }
+        
     }
 }
